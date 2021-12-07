d9569dee6170a246794ef5afa6552fc1d367831e
Ajulibe <akachi_benji@yahoo.com>
2021-12-07 09:29:29 +0100

    adding

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,10 +5,14 @@ module.exports = {
   // Lint then format TypeScript and JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
     `yarn eslint --fix ${filenames.join(" ")}`,
-    `yarn prettier --write ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`
   ],
 
   // Format MarkDown and JSON
-  "**/*.(md|json)": (filenames) =>
-    `yarn prettier --write ${filenames.join(" ")}`,
-}
+  "**/*.(md|json)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
+
+  // Run type-check on changes to TypeScript files
+  "**/*.ts?(x)": () => "yarn type-check",
+  // Run ESLint on changes to JavaScript/TypeScript files
+  "**/*.(ts|js)?(x)": (filenames) => `yarn lint . ${filenames.join(" ")}`
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,36 @@
-import type { NextPage } from "next"
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import Layout from "@src/layout"
-import { Header } from "@src/sections/header"
-import { useEffect, useRef } from "react"
-import dynamic from "next/dynamic"
-import _ from "lodash"
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Layout from "@src/layout";
+import { Header } from "@src/sections/header";
+import { useEffect, useRef } from "react";
+import dynamic from "next/dynamic";
+import _ from "lodash";
 import {
   SectionOne,
   SectionTwo,
   SectionThree,
-  SectionFour,
+  SectionFour
   // Testheader,
   // Apiheader,
-} from "@src/sections"
-import { Footer } from "@src/sections/footer"
-const Pads = dynamic(() => import("@components/pads"))
+} from "@src/sections";
+import { Footer } from "@src/sections/footer";
+const Pads = dynamic(() => import("@components/pads"));
 const Home: NextPage = () => {
-  const hoverBg = useRef<HTMLDivElement>(null)
+  const hoverBg = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    cursorBg()
+    cursorBg();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   const cursorBg = _.debounce(function () {
     document.addEventListener("mousemove", (event) => {
       if (hoverBg.current) {
-        hoverBg.current.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`
+        hoverBg.current.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
       }
-    })
-  }, 500)
+    });
+  }, 500);
 
   return (
     <div className={styles.container}>
@@ -59,11 +59,11 @@ const Home: NextPage = () => {
         <SectionOne />
         <SectionTwo />
         <SectionThree />
-        <SectionFour />
+        {/* <SectionFour /> */}
         <Footer />
       </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
